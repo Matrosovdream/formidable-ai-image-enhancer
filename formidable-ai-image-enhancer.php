a6f1aa7186362f85ad330bb8dd1cf23fcd073a44
Stan Matrosov <matrosovdream@gmail.com>
2026-02-18 21:18:34 +0700

    Shortcode formidable-ai-image-enhancer, changed for a new set of Default prompts

--- formidable-ai-image-enhancer.php
+++ formidable-ai-image-enhancer.php
@@ -24,34 +24,15 @@ require_once 'references.php';
 // Initialize core
 require_once 'classes/FrmImageEnhancerInit.php';
 
+
+
 add_action('init', function() {
     
     if( isset( $_GET['gemini'] ) ) {
 
-        geminiTest();
         exit();
 
     }
 
 });
 
-
-function geminiTest() {
-
-    require_once FRM_AI_BASE_URL . '/gemini-test.php';
-
-    $apiKey = '';
-    $inputFilePath = __DIR__ . '/image-2.jpg';
-
-    $client = new GeminiNanoBananaClient($apiKey);
-
-    $result = $client->processImage(
-        $inputFilePath,
-        'Turn this into cats world.'
-    );
-
-    header('Content-Type: application/json');
-    echo json_encode($result, JSON_PRETTY_PRINT);
-
-
-}
